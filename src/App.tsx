ddae2beae9f358dc88137b2277782eb6f9723c8b
Youkwhan <Youkwhan@gmail.com>
2022-11-08 23:31:14 -0500

    Navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-  hello
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				{/* Navbar needs to be inside Router such that it has access to react-router-dom functonality  */}
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
