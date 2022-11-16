eb999098d4aca624920f279e70a13971d45df07f
Youkwhan <Youkwhan@gmail.com>
2022-11-16 16:54:25 -0500

    CreatePost Form with validation using yup

---
+++ src/pages/create-post/CreateForm.tsx
@@ -0,0 +1,38 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface CreateFormData {
+	title: string;
+	description: string;
+}
+
+export const CreateForm = () => {
+	//Validation
+	const schema = yup.object().shape({
+		title: yup.string().required("Needs a Title"),
+		description: yup.string().required("Needs a Description"),
+	});
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<CreateFormData>({
+		resolver: yupResolver(schema),
+	});
+
+	const onCreatePost = (data: CreateFormData) => {
+		console.log(data);
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onCreatePost)}>
+			<input placeholder="Title..." {...register("title")} />
+			<p style={{ color: "red" }}>{errors.title?.message}</p>
+			<textarea placeholder="Description..." {...register("description")} />
+			<p style={{ color: "red" }}>{errors.description?.message}</p>
+			<input type="submit" />
+		</form>
+	);
+};
